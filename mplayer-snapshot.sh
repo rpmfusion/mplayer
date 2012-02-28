6f8eb816fab47fb208042c7408c9a3f88b31158e
Julian Sikorski <belegdol@gmail.com>
2012-02-28 18:36:31 +0000

    Re-added files deleted during retiring process

---
+++ mplayer-snapshot.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+tmp=$(mktemp -d)
+
+trap cleanup EXIT
+cleanup() {
+    set +e
+    [ -z "$tmp" -o ! -d "$tmp" ] || rm -rf "$tmp"
+}
+
+unset CDPATH
+pwd=$(pwd)
+svn=$(date +%Y-%m-%d)
+svn=2012-02-04
+dirname=mplayer-export-$svn
+mplayer_rev={$svn}
+#mplayer_rev=HEAD
+
+cd "$tmp"
+svn checkout --ignore-externals -r ${mplayer_rev} svn://svn.mplayerhq.hu/mplayer/trunk $dirname
+cd $dirname
+
+rm -rf libdvdcss libswscale
+
+svn_revision=`LC_ALL=C svn info 2> /dev/null | grep Revision | cut -d' ' -f2`
+sed -i -e 's/\(SVN-r[0-9]* \)/\1rpmfusion /' -e "s/UNKNOWN/SVN-r$svn_revision/" version.sh
+find . -type d -name .svn -print0 | xargs -0r rm -rf
+cd ..
+tar jcf "$pwd"/$dirname.tar.bz2 $dirname
+cd - >/dev/null
